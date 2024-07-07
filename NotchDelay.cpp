ab99fbd8de06befc68d8819d1d061523fb12c45a
拓海 岩崎 <metria2018@icloud.com>
2024-07-07 17:12:33 +0900

    プロジェクト ファイルを追加します。

---
+++ NotchDelay.cpp
@@ -0,0 +1,112 @@
+// Ats.cpp : DLL アプリケーション用のエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "atsplugin.h"
+#include "doorCloseingSecurity.h"
+#include "NotchDelay.h"
+#include <stdio.h>
+#include <cmath>
+
+BOOL APIENTRY DllMain( HANDLE hModule, 
+                       DWORD  ul_reason_for_call, 
+                       LPVOID lpReserved
+					 )
+{
+
+    return TRUE;
+}
+
+ATS_API int WINAPI GetPluginVersion()
+{
+	return ATS_VERSION;
+}
+
+ATS_API void WINAPI SetVehicleSpec(ATS_VEHICLESPEC vehicleSpec)
+{
+	g_svcBrake = vehicleSpec.BrakeNotches;
+	g_emgBrake = g_svcBrake + 1;
+}
+
+ATS_API void WINAPI Initialize(int brake)
+{
+	g_speed = 0;
+}
+
+ATS_API ATS_HANDLES WINAPI Elapse(ATS_VEHICLESTATE vehicleState, int *panel, int *sound)
+{
+	g_speed = vehicleState.Speed;
+	g_output.Brake = g_brakeNotch;
+	g_output.Power = g_powerNotch;
+	g_output.Reverser = g_doorCloseingSecurity.main(g_pilotlamp, g_reverser); // 戸閉保安出力
+	main(&panel[200], vehicleState.Time, panel[200]);
+	main(&panel[201], vehicleState.Time, panel[201]);
+    return g_output;
+}
+
+ATS_API void WINAPI SetPower(int notch)
+{
+	g_powerNotch = notch;
+}
+
+ATS_API void WINAPI SetBrake(int notch)
+{
+	g_brakeNotch = notch;
+}
+
+ATS_API void WINAPI SetReverser(int pos)
+{
+	g_reverser = pos;
+}
+
+ATS_API void WINAPI KeyDown(int atsKeyCode)
+{
+}
+
+
+ATS_API void WINAPI KeyUp(int hornType)
+{
+}
+
+ATS_API void WINAPI HornBlow(int atsHornBlowIndex)
+{
+}
+
+ATS_API void WINAPI DoorOpen()
+{
+	g_pilotlamp = false;
+}
+
+ATS_API void WINAPI DoorClose()
+{
+	g_pilotlamp = true;
+}
+
+ATS_API void WINAPI SetSignal(int signal)
+{
+}
+
+ATS_API void WINAPI SetBeaconData(ATS_BEACONDATA beaconData)
+{
+}
+
+ATS_API void WINAPI Load() 
+{
+}
+
+ATS_API void WINAPI Dispose() 
+{
+}
+
+void main(int* pTargetIndex, int CurrentTime, int Value) {
+	int ValueSave = Value;
+	int ChangeTime = NULL;
+	int ValueOld = NULL;
+	if (ValueSave != ValueOld) {
+		ChangeTime = CurrentTime;
+	}
+	if (CurrentTime - ChangeTime >= 750) {
+		*pTargetIndex = Value;
+	}
+	ValueOld = ValueSave;
+}
